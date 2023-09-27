624402585006d847e8a6a3a17c1829d8fd865020
Gabriel Uribe Gomez <info@colsysnet.com>
2023-09-26 20:26:20 -0500

    26 sept se crea controlador para leer productos, se explica try y catch

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -1,16 +1,44 @@
 import { pool } from '../db.js'
 
 export const crearProducto = async (req, res) => {
-    let nombre = req.body.nombre;
-    let precio = req.body.precio;
-    let marca = req.body.marca;
-    //console.log(`Se recibio el producto de id ${id}, nombre: ${nombre} de marca ${marca}, con precio: ${precio}`)
 
-    let sql = 'INSERT INTO `tienda`.`productos` (`nombre`, `precio`, `marca`) VALUES (?,?,?);'
-    let datos = [nombre, precio, marca];
+    try {
+        let nombre = req.body.nombre;
+        let precio = req.body.precio;
+        let marca = req.body.marca;
 
-    let rta = await pool.query(sql, datos)
-    console.log(rta)
+        //console.log(`Se recibio el producto de id ${id}, nombre: ${nombre} de marca ${marca}, con precio: ${precio}`)
 
+        let sql = 'INSERT INTO `tienda`.`productos` (`nombre`, `precio`, `marca`) VALUES (?,?,?);'
+        let datos = [nombre, precio, marca];
 
-}   
+        let rta = await pool.query(sql, datos)
+        console.log(rta[0])
+
+        if (rta[0].affectedRows == 1) {
+            return res.status(200).json({ message: 'Producto guardado con exito' })
+        }
+    } catch (e) {
+        console.log(`ocurrio un error de mysql ${e}`)
+        return res.status(500).json({ message: `Ocurrio un error de MySQL, mensaje: ${e.sqlMessage}` })
+    }
+}
+
+export const consultarProductoPorID = async (req, res) => {
+    try {
+        const ids = req.params.id
+        const sql = 'SELECT * FROM productos WHERE id = ?'
+        const datos = [id]
+        const rtaSql = await pool.query(sql, datos)
+        let arraRta = rtaSql[0]
+        if (arraRta.length > 0) {
+            let producto = arraRta[0]
+            return res.status(200).json(producto)
+        } else {
+            res.status(204).json()
+        }
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({ message: `Error del servidor, ${e}` })
+    }
+} 
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { crearProducto } from '../controllers/productos.controller.js'
+import { crearProducto, consultarProductoPorID } from '../controllers/productos.controller.js'
 
 const router = Router();
 
+router.get('/producto/id/:id/', consultarProductoPorID)
 router.post('/producto', crearProducto)
 
 export default router
