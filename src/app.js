b4545a112106b2c0e852a846b8e943e5f1e326f2
Gabriel Uribe Gomez <info@colsysnet.com>
2024-02-24 20:09:00 -0500

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from 'cors';
+import cors from 'cors';;
 
 //aca importamos las rutas que creamos en la carpeta routes
 import productoRoutes from './routes/productos.routes.js';
